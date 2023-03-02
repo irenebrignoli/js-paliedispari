59a9f0e1beb5e2dd5d2f0d59571e946111ec878b
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-02 17:50:59 +0100

    Inizio esercizio

---
+++ Palindromo/js/main.js
@@ -0,0 +1,27 @@
+
+
+const parola = prompt('Inserisci parola');
+
+console.log(parola);
+
+//const risultatoPalindromo = palindromo(parola);
+
+
+
+// for(let i = 0; i <= parola.length - 1; i++){
+
+//   console.log(parola[i]);
+// }
+
+
+
+
+
+
+function palindromo (parolaDaControllare){
+
+  for(let i = 0; i <= parolaDaControllare.length - 1; i++){
+
+    console.log(parolaDaControllare[i]);
+  }
+}
